80aac961675351ec1e81af85b5ec74d608b74915
Ishu kumar <83811024+Ishu214214@users.noreply.github.com>
2023-08-17 17:38:51 +0530

    Delete a.py

--- PDF conversion/a.py
+++
@@ -1,9 +0,0 @@
-from pdf2docx import Converter
-import os
-
-pdf_file = os.path.abspath('c:\\Users\\ishuk\\Desktop\\PDF conversion\\static\\uploads\\abc.pdf')
-docx_file = os.path.abspath('c:\\Users\\ishuk\\Desktop\\PDF conversion\\static\\uploads\\abc.docx')
-
-cv=Converter(pdf_file)
-cv.convert(docx_file)
-cv.close()
